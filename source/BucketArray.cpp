ad486e2fed7706926b91af6d915f8129edcfa632
Andrew Valentine <piroshky@users.noreply.github.com>
2023-11-08 15:52:35 -0800

    Improve BucketArray

--- source/BucketArray.cpp
+++ source/BucketArray.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cstdint>
 
 struct BucketLocator {
   int bucket_index;
@@ -20,7 +21,7 @@ template<typename T, int items_per_bucket>
 struct BucketArray {
   int count = 0;
 
-  int    bucket_count = 0;  
+  int bucket_count = 0;  
   std::vector<Bucket<T, items_per_bucket> *> all_buckets;
   
   int unfull_bucket_count = 0;
@@ -28,7 +29,16 @@ struct BucketArray {
 
   iterator<T, items_per_bucket> begin() {return iterator{this};}
   iterator<T, items_per_bucket> end()   {return iterator{this, count};}
-  
+
+  // public
+  BucketLocator  add(T *obj);
+  T             *request();   // gives a pointer an unused bucket slot and initializes the memory with default constructor.
+  T             *get(int index);
+  void           remove(T *obj);
+
+  // internal
+  void add_bucket();
+  void debug();  
 };
 
 template<typename T, int items_per_bucket>
@@ -66,7 +76,7 @@ struct iterator {
   }
 
   reference operator*() const { return *ptr; }
-  pointer operator->() { return ptr; }
+  pointer   operator->()      { return ptr; }
     
   iterator& operator++() {
     index += 1;
@@ -91,24 +101,17 @@ struct iterator {
   friend bool operator!= (const iterator& a, const iterator& b) { return a.index != b.index; }
 };
 
-template<typename T, int items_per_bucket>
-void BucketArray_add_bucket(BucketArray<T, items_per_bucket> *array) {
-  Bucket<T, items_per_bucket> *new_bucket = new Bucket<T, items_per_bucket>;
-  new_bucket->bucket_index = array->bucket_count;
-    
-  array->all_buckets.push_back(new_bucket);
-  array->bucket_count += 1;
-  array->unfull_buckets.push_back(new_bucket);  
-  array->unfull_bucket_count += 1;
-}
 
+//// public BucketArray functions
+
+// add(T *obj)
 template<typename T, int items_per_bucket>
-BucketLocator BucketArray_add(BucketArray<T, items_per_bucket> *array, T *obj) {
-  if (array->unfull_bucket_count == 0) {
-    BucketArray_add_bucket(array);
+BucketLocator BucketArray<T, items_per_bucket>::add(T *obj) {
+  if (unfull_bucket_count == 0) {
+    add_bucket();
   }
 
-  Bucket<T, items_per_bucket> *bucket = array->unfull_buckets[0];
+  Bucket<T, items_per_bucket> *bucket = unfull_buckets[0];
   
   int index = -1;
   for (int i = 0; i < items_per_bucket; ++i) {
@@ -121,19 +124,50 @@ BucketLocator BucketArray_add(BucketArray<T, items_per_bucket> *array, T *obj) {
   bucket->fill_mask[index] = true;
   bucket->data[index] = *obj;
   bucket->count += 1;
-  array->count  += 1;
+  count  += 1;
 
   if (bucket->count == items_per_bucket) {
-    array->unfull_buckets.erase(array->unfull_buckets.begin());
-    array->unfull_bucket_count -= 1;
+    unfull_buckets.erase(unfull_buckets.begin());
+    unfull_bucket_count -= 1;
   }
 
   return BucketLocator{bucket->bucket_index, index};
 }
 
+// request();
 template<typename T, int items_per_bucket>
-T *BucketArray_get(BucketArray<T, items_per_bucket> *array, int index) {
-  for (auto b : array->all_buckets) {
+T *BucketArray<T, items_per_bucket>::request() {
+  if (unfull_bucket_count == 0) {
+    add_bucket();
+  }
+
+  Bucket<T, items_per_bucket> *bucket = unfull_buckets[0];
+  
+  int index = -1;
+  for (int i = 0; i < items_per_bucket; ++i) {
+    if (!bucket->fill_mask[i]) {
+      index = i;
+      break;
+    }
+  }
+
+  bucket->fill_mask[index] = true;
+  bucket->data[index] = T{};
+  bucket->count += 1;
+  count  += 1;
+
+  if (bucket->count == items_per_bucket) {
+    unfull_buckets.erase(unfull_buckets.begin());
+    unfull_bucket_count -= 1;
+  }
+
+  return &(bucket->data[index]);
+}
+
+// get(int index);
+template<typename T, int items_per_bucket>
+T *BucketArray<T, items_per_bucket>::get(int index) {
+  for (auto b : all_buckets) {
     if (b->count >= index + 1) {
       int ix = -1;
       for(int i = 0; i < items_per_bucket; ++i) {
@@ -153,23 +187,68 @@ T *BucketArray_get(BucketArray<T, items_per_bucket> *array, int index) {
   return NULL;
 }
 
+// remove(T *obj);
 template<typename T, int items_per_bucket>
-void BucketArray_debug(BucketArray<T, items_per_bucket> *array) {
+void BucketArray<T, items_per_bucket>::remove(T *obj) {
+  for (auto b : all_buckets) {
+    if ((uintptr_t)b < (uintptr_t)obj  &&  (uintptr_t)obj < (uintptr_t)(b + 1)) {
+      int calc_index = ((uintptr_t)obj - (uintptr_t)b->data) / (sizeof(T));      
+      b->fill_mask[calc_index] = false;
+
+      if(b->count == items_per_bucket) {
+	unfull_bucket_count += 1;
+	unfull_buckets.push_back(b);
+      }	
+      
+      b->count -= 1;
+      count -= 1;
+      break;
+    }
+  }
+}
+
+
+//// internal functions
+
+// add_bucket();
+template<typename T, int items_per_bucket>
+void BucketArray<T, items_per_bucket>::add_bucket() {
+  Bucket<T, items_per_bucket> *new_bucket = new Bucket<T, items_per_bucket>;
+  new_bucket->bucket_index = bucket_count;
+    
+  all_buckets.push_back(new_bucket);
+  bucket_count += 1;
+  unfull_buckets.push_back(new_bucket);  
+  unfull_bucket_count += 1;    
+}
+
+// debug();
+template<typename T, int items_per_bucket>
+void BucketArray<T, items_per_bucket>::debug() {
   printf("---------------------------------------------\n"
 	 "     items_per_bucket = %d\n"
-	 "                count = %d\n"
+	 "                count = %d\n\n"
 	 "         bucket_count = %d\n"
-	 "  unfull_bucket_count = %d\n",
+	 "     full_bucket size = %d\n\n"
+	 "  unfull_bucket_count = %d\n"
+	 "   unfull_bucket size = %d\n",
 	 items_per_bucket,
-	 array->count,
-	 array->bucket_count,
-	 array->unfull_bucket_count);
+	 count,
+	 bucket_count,
+	 all_buckets.size(),
+	 unfull_bucket_count,
+	 unfull_buckets.size());
 
-  if (array->bucket_count > 0) {
+  if (bucket_count > 0) {
     printf("\nall buckets:\n");
-    for (int i = 0; i < array->bucket_count; ++i) {
-      Bucket<T, items_per_bucket> *b = array->all_buckets[i];
-      printf("   bucket_index = %d\n", b->bucket_index);
+    for (int i = 0; i < bucket_count; ++i) {
+      Bucket<T, items_per_bucket> *b = all_buckets[i];
+      printf("   bucket_index = %d, (%d / %d)\n", b->bucket_index, b->count, items_per_bucket);
+      printf("     [");
+      for(auto fill : b->fill_mask) {
+	printf(" %c", fill ? 'X' : '_');
+      }
+      printf(" ]\n");
     }
   }
 }
